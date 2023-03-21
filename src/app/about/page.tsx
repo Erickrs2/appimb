5523a283d057ad8b5181c3fca6a2bb7ece70dd5d
Your Name <you@example.com>
2023-03-21 14:26:22 -0300

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl space-y-4 mx-auto p-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>
+            About
+        </h1>
+        <p>
+            some text about the business
+        </p>
+    </div>
+  )
+}
